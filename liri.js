ee51dac6810b9be3cbbfcdd2a652bf1608dcca74
Joshua <jdkelly2015@gmail.com>
2022-03-07 20:39:37 -0500

    i have been able to make successful requests with the Spotify NPM package. However i am not sure how to parse the data to find what i need.

--- liri.js
+++ liri.js
@@ -1,3 +1,21 @@
+// read and set any environment variables with the dotenv package
 require('dotenv').config();
+const Spotify = require('node-spotify-api');
+const axios = require('axios');
+// import the keys.js file
 const keys = require('./keys.js');
+
+// access keys information
 const spotify = new Spotify(keys.spotify);
+
+spotify
+  .search({ type: 'track', query: 'Best Day Ever', limit: 1 })
+  .then((res) => console.log(res))
+  .catch((err) => new Error(err));
+// command for bands in town
+// node liri.js concert-this <artist/band name here>
+
+// url for bands in town
+// https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
+
+// url for spotify
