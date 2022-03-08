252a1adb1e7cb1d2cc01ede25541660fdcae0196
Joshua <jdkelly2015@gmail.com>
2022-03-07 20:50:59 -0500

    i able to hardcode Spotify Queries. need to figure out we planned to use command line to send request

--- liri.js
+++ liri.js
@@ -1,17 +1,21 @@
 // read and set any environment variables with the dotenv package
 require('dotenv').config();
+
+// require Spotify Package installed in npm
 const Spotify = require('node-spotify-api');
-const axios = require('axios');
-// import the keys.js file
+
+// import the keys.js file that holds SPOTIFY API info
 const keys = require('./keys.js');
 
 // access keys information
 const spotify = new Spotify(keys.spotify);
 
+// search Spotify
 spotify
-  .search({ type: 'track', query: 'Best Day Ever', limit: 1 })
+  .search({ type: 'track', query: 'The Sharpest Lives', limit: 1 })
   .then((res) => console.log(res))
-  .catch((err) => new Error(err));
+  .catch((err) => console.log(new Error(err)));
+
 // command for bands in town
 // node liri.js concert-this <artist/band name here>
 
