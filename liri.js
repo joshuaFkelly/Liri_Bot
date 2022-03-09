9fb23dea405cb1b8c238e6180615318ca72a329b
Joshua <jdkelly2015@gmail.com>
2022-03-09 14:09:15 -0500

    i have to refactor all my code and put them in to functions so i can just input whatever is on the random.txt file as args in the functions

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 // read and set any environment variables with the dotenv package
 require('dotenv').config();
 
+// import fs package to read random.txt
+const fs = require('fs');
 // import Axios for get requests
 const axios = require('axios');
 
@@ -12,6 +14,7 @@ const moment = require('moment');
 
 // import the keys.js file that holds SPOTIFY API info
 const keys = require('./keys.js');
+const exp = require('constants');
 
 // access keys information
 const spotify = new Spotify(keys.spotify);
@@ -21,10 +24,13 @@ const bandsInTown = keys.bandsInTown;
 // callback to display spotify info
 const displaySpotify = (data) => {
   console.log(`
-  Artist(s): ${data.artists[0].name}
-  Song Name: ${data.name}
-  Preview Link: ${data.preview_url}
-  Album Name: ${data.album.name}
+Artist(s): ${data.artists[0].name}
+
+Song Name: ${data.name}
+
+Preview Link: ${data.preview_url}
+
+Album Name: ${data.album.name}
   `);
 };
 
@@ -32,36 +38,36 @@ const displaySpotify = (data) => {
 const displayConcert = (events) => {
   events.forEach((event, i) => {
     console.log(`
-          ---------------------------------------------------------
-          id: ${i}
+---------------------------------------------------------
+id: ${i}
 
-          Venue Name: ${event.venue.name}
+Venue Name: ${event.venue.name}
 
-          Venue Location: ${event.venue.location}
+Venue Location: ${event.venue.location}
 
-          Date/Time: ${moment(event.datetime).format('MM/DD/YYYY hh:mm:ss')}`);
+Date/Time: ${moment(event.datetime).format('MM/DD/YYYY hh:mm:ss')}`);
   });
 };
 
 // Callback to display movie info
 const displayMovie = (data) => {
   console.log(`
-          Title: ${data.Title}
+Title: ${data.Title}
 
-          Release year: ${data.Year}
+Release year: ${data.Year}
 
-          Movie Ratings: 
-            - imdbRating: ${data.imdbRating}
-            - ${data.Ratings[0].Source}: ${data.Ratings[0].Value}
-            - ${data.Ratings[1].Source}: ${data.Ratings[1].Value}
+Movie Ratings: 
+  - imdbRating: ${data.imdbRating}
+  - ${data.Ratings[0].Source}: ${data.Ratings[0].Value}
+  - ${data.Ratings[1].Source}: ${data.Ratings[1].Value}
 
-          Country: ${data.Country}
+Country: ${data.Country}
 
-          Language: ${data.Language}
+Language: ${data.Language}
 
-          Plot: ${data.Plot}
+Plot: ${data.Plot}
 
-          Actors: ${data.Actors}`);
+Actors: ${data.Actors}`);
 };
 
 // argument variables from command line
@@ -118,6 +124,19 @@ switch (expr) {
       .then(displayMovie);
     break;
 
+  case 'do-what-it-says':
+    fs.readFile('./random.txt', 'utf-8', (err, data) => {
+      if (err) {
+        console.log(new Error(err));
+      }
+      console.log(data);
+      const randomTxt = data.split(',');
+      const randomCommand = randomTxt[0];
+      const randomValue = randomTxt[1];
+      console.log('Command: ', randomCommand);
+      console.log('Value: ', randomValue);
+    });
+    break;
   default:
     console.log(`Could not evaluate command: ${expr}`);
 }
