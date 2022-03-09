d8d8bd6e4d0b70ab666300b374c58ce34dbdedbb
Joshua <jdkelly2015@gmail.com>
2022-03-09 10:29:40 -0500

    got spotify to work great! i feel like theres better ways of doing this stuff tho like using npm packages. the more i think about it tho i feel thats the case for everything. there will almost awlays be an npm package to make things easier. the question is, do i use tons of random and new(to me) npm packages or do i stick with vanila JS?

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
-console.log('this is loaded');
+console.log('API Key Loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
+};
+
+exports.bandsInTown = {
+  id: process.env.APP_ID,
 };
--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // read and set any environment variables with the dotenv package
 require('dotenv').config();
-
+const axios = require('axios');
 // require Spotify Package installed in npm
 const Spotify = require('node-spotify-api');
 
@@ -9,17 +9,40 @@ const keys = require('./keys.js');
 
 // access keys information
 const spotify = new Spotify(keys.spotify);
+const concertAPIkey = keys.bandsInTown;
+// Spotify promise callback
+const displaySpotify = (res) => {
+  console.log('Artist(s): ', res.tracks.items[0].artists[0].name);
+  console.log('Song Name: ', res.tracks.items[0].name);
+  console.log('Preview Link: ', res.tracks.items[0].preview_url);
+  console.log('Album Name: ', res.tracks.items[0].album.name);
+};
 
-// search Spotify
-spotify
-  .search({ type: 'track', query: 'The Sharpest Lives', limit: 1 })
-  .then((res) => console.log(res))
-  .catch((err) => console.log(new Error(err)));
+const expr = process.argv.slice(2, 3).toString();
+const query = process.argv.slice(3).join(' ').toString();
+switch (expr) {
+  case 'spotify-this-song':
+    // search Spotify
+    spotify
+      .search({ type: 'track', query: query, limit: 1 })
+      .then(displaySpotify)
+      .catch((err) => console.log(new Error(err)));
+    break;
+  case 'concert-this':
+    axios
+      .get(
+        `https://rest.bandsintown.com/artists/${query}/events?${concertAPIkey}`
+      )
+      .then((res) => {
+        // handle success
+        console.log(res);
+      })
+      .catch((err) => {
+        // handle err
+        console.log(err);
+      });
 
-// command for bands in town
-// node liri.js concert-this <artist/band name here>
-
-// url for bands in town
-// https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp
-
-// url for spotify
+    break;
+  default:
+    console.log(`Could not evaluate command: ${expr}`);
+}
