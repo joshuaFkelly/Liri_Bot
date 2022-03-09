6e2d4b7e3c6bb49a1aacf182865d5b00cb99cb80
Joshua <jdkelly2015@gmail.com>
2022-03-09 11:19:52 -0500

    im able to get the bands in town api workiing. i tried hiding the api key but i couldnt get it to work. i would like it to be hidden inside the .env file with my spotify key. i was able to format the date from the bands in town response using momentJS. about half wayw through completing this CLI App

--- keys.js
+++ keys.js
@@ -1,10 +1,5 @@
 console.log('API Key Loaded');
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET,
-};
-
-exports.bandsInTown = {
-  id: process.env.APP_ID,
 };
--- liri.js
+++ liri.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 const axios = require('axios');
 // require Spotify Package installed in npm
 const Spotify = require('node-spotify-api');
-
+// import momentJS to format dat/time
+const moment = require('moment');
 // import the keys.js file that holds SPOTIFY API info
 const keys = require('./keys.js');
 
 // access keys information
 const spotify = new Spotify(keys.spotify);
-const concertAPIkey = keys.bandsInTown;
+
 // Spotify promise callback
 const displaySpotify = (res) => {
   console.log('Artist(s): ', res.tracks.items[0].artists[0].name);
@@ -19,29 +20,46 @@ const displaySpotify = (res) => {
 };
 
 const expr = process.argv.slice(2, 3).toString();
-const query = process.argv.slice(3).join(' ').toString();
+const song = process.argv.slice(3).join(' ').toString();
+const band = process.argv.slice(3).join('').toString();
+console.log(band);
+
 switch (expr) {
   case 'spotify-this-song':
     // search Spotify
     spotify
-      .search({ type: 'track', query: query, limit: 1 })
+      .search({ type: 'track', query: song, limit: 1 })
       .then(displaySpotify)
       .catch((err) => console.log(new Error(err)));
     break;
   case 'concert-this':
     axios
       .get(
-        `https://rest.bandsintown.com/artists/${query}/events?${concertAPIkey}`
+        `https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`
       )
       .then((res) => {
         // handle success
-        console.log(res);
+        return res.data;
       })
       .catch((err) => {
         // handle err
         console.log(err);
+      })
+      .then((data) => {
+        return data;
+      })
+      .then((events) => {
+        events.forEach((event, i) => {
+          console.log('-----------------------------------------');
+          console.log('id: ', i);
+          console.log('Venue Name: ', event.venue.name);
+          console.log('Venue Location: ', event.venue.location);
+          console.log(
+            'Date/Time: ',
+            moment(event.datetime).format('MM/DD/YYYY hh:mm:ss')
+          );
+        });
       });
-
     break;
   default:
     console.log(`Could not evaluate command: ${expr}`);
